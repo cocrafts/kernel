d3f39e28fc92d39d5c2238ddbb640d80441b3098
kayaba <kayaba.fluctlight@gmail.com>
2022-08-26 21:32:33 +0700

    Flag and StormGate icon

--- packages/icons/src/index.tsx
+++ packages/icons/src/index.tsx
@@ -1,1 +1,2 @@
 export * from './feather';
+export * from './storm';
---
+++ packages/icons/src/storm/index.ts
@@ -0,0 +1,7 @@
+import Flag from './Flag';
+import StormGate from './StormGate';
+
+export const stormIcons = {
+	Flag,
+	StormGate,
+};
