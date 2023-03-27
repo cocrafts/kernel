debdfd79a60fca8904d66eb310c1671f5ecdf46a
kayaba <fluctlight-kayaba@gmail.com>
2023-03-27 22:16:22 +0700

    eslint-config with Next.js plugin

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -9,6 +9,7 @@ module.exports = {
 	extends: [
 		'eslint:recommended',
 		'plugin:import/typescript',
+		'plugin:@next/next/recommended',
 		'plugin:@typescript-eslint/recommended',
 		'prettier',
 	],
